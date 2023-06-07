38a81b71b9ca9b40600b38af3156c10b2585b842
pheistman <eapreko@icloud.com>
2023-06-07 14:24:33 +0100

    install git-extras, purge libreoffice

--- dcv_ubuntu_installation.sh
+++ dcv_ubuntu_installation.sh
@@ -361,7 +361,7 @@ sudo tee /etc/apt/sources.list.d/hashicorp.list
 echo "Install Ansible"
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
-sudo apt install terminator -y
+sudo apt install git-extras terminator -y
 
 # Finish installing Terraform, pyhton3-pip and Terminator
 echo "Finish installing Terraform, pyhton3-pip and Terminator"
@@ -380,6 +380,9 @@ source /home/ubuntu/.bashrc
 # Cleanup: Delete all .deb files
 echo "Delete all .deb files and aws zip file"
 rm *.deb awscliv2.zip
+
+# Purge libreoffice installation
+sudo apt purge libreoffice* -y
 
 # Reboot the system
 sudo reboot
